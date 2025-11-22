e9f4f1def1ecd55810e213e8155cbe747cd20e5b
riya-fathima-2005 <riyaf8052@gmail.com>
2025-11-22 15:25:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function btnClick(value) {
+    document.getElementById("screen").value += value;
+}
+function clearScreen() {
+    document.getElementById("screen").value = "";
+}
+
+function calculate() {
+    let result = eval(document.getElementById("screen").value);
+    document.getElementById("screen").value = result;
+}
